92aa5efae3d3aa87d4cab97255640702b3b55bd0
JoeM-RP <JMeyer@rightpoint.com>
2017-09-04 11:07:55 -0500

    Init

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace MvvmLight
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MvvmLightPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MvvmLightPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MvvmLight
+{
+    public partial class MvvmLightPage : ContentPage
+    {
+        public MvvmLightPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
